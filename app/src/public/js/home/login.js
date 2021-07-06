4d0b075f674ebb18e6da5479d5b3a47a0153dfd7
kimkyoungcheon <rudcjs32@gmail.com>
2021-07-07 00:02:28 +0900

    로그인을 요청하는 fetch생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -12,4 +12,12 @@ function login(){
         psword: psword.value,
     };
     console.log(req)
+    console.log(JSON.stringify(req))
+    fetch("/login",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(req)
+    })
 }
