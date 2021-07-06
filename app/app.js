e759a5bbf1b373dda108f210efd8a4eef4845072
kimkyoungcheon <rudcjs32@gmail.com>
2021-07-06 23:35:06 +0900

    폴더구조 최적화

---
+++ app/app.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const express  = require('express');
+const app = express();
+
+const home = require("./src/routes/home");
+
+
+//앱 세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+
+app.use("/",home);
+
+module.exports = app;
